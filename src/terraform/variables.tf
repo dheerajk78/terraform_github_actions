c690d9aae35723d9d9d9b6f3b0ee9c7deb408d20
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2024-11-14 12:16:24 +1100

    Create variables.tf

---
+++ src/terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "application_name" {
+  type = string
+}
+variable "environment_name" {
+  type = string
+}
+variable "primary_region" {
+  type = string
+}
+variable "vpc_cidr_block" {
+  type = string
+}
+variable "bucket" {
+  type = string
+}
+variable "state_key" {
+  type = string
+}
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
