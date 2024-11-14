cfb02aacf5f47ff02812c5d76c84d891e72777d1
dheerajk78 <44789978+dheerajk78@users.noreply.github.com>
2024-11-14 12:15:42 +1100

    Create provider.tf

---
+++ src/terraform/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.17"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.2"
+    }
+  }
+  backend "s3" {
+    bucket = var.bucket
+    key = var.state_key
+    region = var.primary_region
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = var.primary_region
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+}
